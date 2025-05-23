4945d3b23f9ce2494bf9cf98673ec0cf1f95c6d7
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-05-24 02:35:26 +0500

    Set DEBUG to False for production readiness in settings.py

--- shoppinglyW/settings.py
+++ shoppinglyW/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8z(3$p7!i_i*#f6946x0)*l=xs4kx36+ajs74i@a0%73ao$f&2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shophoria128.pythonanywhere.com', 'localhost', '127.0.0.1']
 
